b81c0a919e0275ba1520eb594ad0d20e227d1dcf
krrsalmane <karrounsalmane@gmail.com>
2025-07-08 17:11:16 +0100

    Create un Brief

--- Brief-Service/src/main/java/org/example/briefservice/controller/BriefController.java
+++ Brief-Service/src/main/java/org/example/briefservice/controller/BriefController.java
@@ -1,4 +1,28 @@
 package org.example.briefservice.controller;
 
+import org.example.briefservice.model.Brief;
+import org.example.briefservice.service.BriefService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/briefs")
+
 public class BriefController {
+
+
+    private final BriefService briefService;
+
+    public BriefController(BriefService briefService) {
+        this.briefService = briefService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Brief> create(@RequestBody Brief brief) {
+        return ResponseEntity.ok(briefService.saveBrief(brief));
+    }
 }
--- Brief-Service/src/main/java/org/example/briefservice/service/BriefService.java
+++ Brief-Service/src/main/java/org/example/briefservice/service/BriefService.java
@@ -1,4 +1,20 @@
 package org.example.briefservice.service;
 
+import org.example.briefservice.model.Brief;
+import org.example.briefservice.repository.BriefRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BriefService {
+
+    private final BriefRepository briefRepository;
+
+    public BriefService(BriefRepository briefRepository) {
+        this.briefRepository = briefRepository;
+    }
+
+    public Brief saveBrief(Brief brief) {
+        return briefRepository.save(brief);
+    }
 }
