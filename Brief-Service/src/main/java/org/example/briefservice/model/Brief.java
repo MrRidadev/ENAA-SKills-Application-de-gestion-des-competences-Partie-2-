197c07dd4413d07f97f90ce27f3946e4c21a6885
krrsalmane <karrounsalmane@gmail.com>
2025-07-08 11:44:39 +0100

    create the model

--- Brief-Service/src/main/java/org/example/briefservice/model/Brief.java
+++ Brief-Service/src/main/java/org/example/briefservice/model/Brief.java
@@ -1,4 +1,54 @@
 package org.example.briefservice.model;
 
+import java.util.Date;
+
 public class Brief {
+
+    private String titre;
+    private String description;
+    private Date dateDebut;
+    private Date dateFin;
+
+
+    public Brief() {
+    }
+
+    public Brief(String titre, String description, Date dateDebut, Date dateFin) {
+        this.titre = titre;
+        this.description = description;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
 }
