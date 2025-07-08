9afcf4a972f96eae7f748e4308427b5daf88a5c4
ridaDev <kiratterida@gmail.com>
2025-07-08 11:53:04 +0100

    créer modele apprenant

--- Apprenant-Service/src/main/java/org/example/apprenantservice/Modeles/Apprernat.java
+++ Apprenant-Service/src/main/java/org/example/apprenantservice/Modeles/Apprernat.java
@@ -1,4 +1,39 @@
 package org.example.apprenantservice.Modeles;
 
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
 public class Apprernat {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nomComplet;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomComplet() {
+        return nomComplet;
+    }
+
+    public void setNomComplet(String nomComplet) {
+        this.nomComplet = nomComplet;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
