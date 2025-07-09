badec3fb00b779ae57e518b121926333c7e3813f
krrsalmane <karrounsalmane@gmail.com>
2025-07-09 10:53:11 +0100

    implement test unitaire

---
+++ Brief-Service/src/test/java/org/example/briefservice/service/BriefServiceTest.java
@@ -0,0 +1,27 @@
+package org.example.briefservice.service;
+
+import org.example.briefservice.model.Brief;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BriefServiceTest {
+
+    @Autowired
+    private BriefService briefService;
+
+
+
+    @Test
+    void testGetAllBriefs() {
+        List<Brief> briefs = briefService.getAllBriefs();
+        assertNotNull(briefs);         // Not null
+        assertTrue(briefs.size() >= 0); // Can be empty or more
+    }
+}
