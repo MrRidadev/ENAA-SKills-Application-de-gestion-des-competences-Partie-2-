80ca7b34a7caa38e9406885306ad059529651535
krrsalmane <karrounsalmane@gmail.com>
2025-07-10 16:16:22 +0100

    inplement les test unitaire

--- Brief-Service/src/test/java/org/example/briefservice/service/BriefServiceTest.java
+++ Brief-Service/src/test/java/org/example/briefservice/service/BriefServiceTest.java
@@ -9,19 +9,48 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class BriefServiceTest {
-
-    @Autowired
-    private BriefService briefService;
-
-
+@Autowired
+private BriefService briefService;
 
     @Test
-    void testGetAllBriefs() {
+    void saveBrief() {
+
+        Brief brief = new Brief();
+        brief.setTitre("titre breif");
+        brief.setDescription("test description");
+
+        Brief saveBreif = briefService.saveBrief(brief);
+
+        assertNotNull(saveBreif.getId());
+        assertEquals(brief.getTitre(),saveBreif.getTitre());
+    }
+
+    @Test
+    void getAllBriefs() {
+
+        Brief brief = new Brief();
+        brief.setTitre("titre breif");
+        brief.setDescription("test description");
+
+
+        Brief saveBreif = briefService.saveBrief(brief);
+
         List<Brief> briefs = briefService.getAllBriefs();
-        assertNotNull(briefs);         // Not null
-        assertTrue(briefs.size() >= 0); // Can be empty or more
+
+        assertNotNull(briefs);
+        assertTrue(briefs.size()>0);
+
+
+
+    }
+
+    @Test
+    void getBriefById() {
+    }
+
+    @Test
+    void updateBrief() {
     }
 }
