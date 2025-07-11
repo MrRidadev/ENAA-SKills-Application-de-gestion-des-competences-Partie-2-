7c760b96e1f46fdd7a3dd21c46aa73d4a7aa3deb
ridaDev <kiratterida@gmail.com>
2025-07-11 09:44:31 +0100

    créer méthode ajoute validation en controller

--- Validation-Service/src/main/java/org/example/validationservice/Controlles/ValidationController.java
+++ Validation-Service/src/main/java/org/example/validationservice/Controlles/ValidationController.java
@@ -1,4 +1,27 @@
 package org.example.validationservice.Controlles;
 
+import org.example.validationservice.Modeles.Validation;
+import org.example.validationservice.Services.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Validation")
 public class ValidationController {
+
+    @Autowired
+    private ValidationService validationService;
+    public ValidationController(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
+
+    // ajouter validation en controller
+    @PostMapping("/add-validation")
+    public Validation ajoutervalidation(@RequestBody Validation validation) {
+        return validationService.addValidation(validation);
+    }
 }
