aa0ac7e01abab785aa149bbb2f3ac9863f556d68
ridaDev <kiratterida@gmail.com>
2025-07-11 09:40:57 +0100

    créer méthode ajoute validation en service

--- Validation-Service/src/main/java/org/example/validationservice/Services/ValidationService.java
+++ Validation-Service/src/main/java/org/example/validationservice/Services/ValidationService.java
@@ -1,4 +1,22 @@
 package org.example.validationservice.Services;
 
+import org.example.validationservice.Modeles.Validation;
+import org.example.validationservice.Repositorys.ValidationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ValidationService {
+
+    @Autowired
+    private ValidationRepository repository;
+    public ValidationService(ValidationRepository repository) {
+        this.repository = repository;
+    }
+
+
+    // ajouter validation
+    public Validation addValidation(Validation validation) {
+        return repository.save(validation);
+    }
 }
