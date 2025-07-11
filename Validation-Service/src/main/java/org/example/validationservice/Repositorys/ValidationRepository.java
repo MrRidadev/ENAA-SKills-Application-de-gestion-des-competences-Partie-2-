51dc027227c33f867873c32c19f03b2b3933d642
ridaDev <kiratterida@gmail.com>
2025-07-11 09:38:23 +0100

    créer repository

--- Validation-Service/src/main/java/org/example/validationservice/Repositorys/ValidationRepository.java
+++ Validation-Service/src/main/java/org/example/validationservice/Repositorys/ValidationRepository.java
@@ -1,4 +1,7 @@
 package org.example.validationservice.Repositorys;
 
-public interface ValidationRepository {
+import org.example.validationservice.Modeles.Validation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ValidationRepository  extends JpaRepository<Validation, Long> {
 }
