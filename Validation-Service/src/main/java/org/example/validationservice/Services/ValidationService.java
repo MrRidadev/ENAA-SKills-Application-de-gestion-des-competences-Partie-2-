4664157d98820d442d69b82132dd43e238c713c1
krrsalmane <karrounsalmane@gmail.com>
2025-07-21 15:05:41 +0100

    still working on eurika

--- Validation-Service/src/main/java/org/example/validationservice/Services/ValidationService.java
+++ Validation-Service/src/main/java/org/example/validationservice/Services/ValidationService.java
@@ -1,8 +1,10 @@
 package org.example.validationservice.Services;
-import org.example.briefservice.DTO.CompetenceDTO;
+
 import org.example.briefservice.client.CompetenceClient;
+import org.example.validationservice.DTO.CompetenceDTO;
 import org.example.validationservice.Modeles.Validation;
 import org.example.validationservice.Repositorys.ValidationRepository;
+import org.example.validationservice.client.validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +16,13 @@ public class ValidationService {
 
     @Autowired
     private ValidationRepository repository;
+
     public ValidationService(ValidationRepository repository) {
         this.repository = repository;
     }
-
     public Validation addValidation(Validation validation) {
         return repository.save(validation);
     }
-
     public List<Validation> getAllValidations() {
         return repository.findAll();
     }
