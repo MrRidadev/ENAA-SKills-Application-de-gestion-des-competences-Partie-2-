ced355e3f0dff18c45434feaaecce2cc4f69bf8f
ridaDev <kiratterida@gmail.com>
2025-07-21 12:37:19 +0100

    fix erreurs

--- auth-Service/src/main/java/org/example/authservice/repositorys/UserRepository.java
+++ auth-Service/src/main/java/org/example/authservice/repositorys/UserRepository.java
@@ -3,9 +3,8 @@ package org.example.authservice.repositorys;
 import org.example.authservice.modeles.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
+
 
 
 }
